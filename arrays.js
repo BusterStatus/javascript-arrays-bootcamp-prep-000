9073368afc8f0da895b09e1e8de6f15cd2ac7f06
matthew.serre@gmail.com <matthew.serre@gmail.com>
2018-09-12 00:42:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,8 @@ function addElementToBeginningOfArray (array, element) {
   var newArray = array.unshift(element);
   return newArray;
 }
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array;
+}
