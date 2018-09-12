2d8af70b596337b79f2d3576a6b50a6cf93cd6f9
matthew.serre@gmail.com <matthew.serre@gmail.com>
2018-09-12 00:57:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,8 +34,7 @@ function removeElementFromBeginningOfArray (array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray (array) {
-  array.slice(0, array.length - 1);
-  return array;
+  return array.slice(0, array.length - 1);
 }
 
 function removeElementFromEndOfArray (array) {
